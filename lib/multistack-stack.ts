04bd8bd5e91be5d3c4c1f3fe9f9e1cf0dabac96b
Josh Hill <hillj38@gene.com>
2021-08-06 14:54:21 -0500

    finished tut

--- lib/multistack-stack.ts
+++ lib/multistack-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import { BlockPublicAccess } from '@aws-cdk/aws-s3';
+
+interface MultiStackProps extends cdk.StackProps {
+  encryptBucket?: boolean;
+}
 
 export class MultistackStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: MultiStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    if (props && props.encryptBucket) {
+      new s3.Bucket(this, 'MyGroovyBucket', {
+        encryption: s3.BucketEncryption.KMS_MANAGED,
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        removalPolicy: cdk.RemovalPolicy.DESTROY
+      });
+    } else {
+      new s3.Bucket(this, 'MyGroovyBucket', {
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        removalPolicy: cdk.RemovalPolicy.DESTROY
+      })
+    }
   }
 }
